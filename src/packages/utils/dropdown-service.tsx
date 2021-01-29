0309db66858f95463c241e5a0563be577b1aa5d2
iceya <1436098850@qq.com>
2021-01-30 03:18:34 +0800

    feat: 点击优化

--- src/packages/utils/dropdown-service.tsx
+++ src/packages/utils/dropdown-service.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, getCurrentInstance, PropType, reactive, createApp, onMounted, onBeforeUnmount, ref } from 'vue'
+import { computed, defineComponent, getCurrentInstance, PropType, reactive, createApp, onMounted, onBeforeUnmount, ref, provide, inject } from 'vue'
 import './dropdown-service.scss'
 import { defer } from './defer'
 
@@ -6,6 +6,18 @@ interface DropdownServiceOption {
   reference: MouseEvent | HTMLElement;
   content: () => JSX.Element;
 }
+
+const DropdownServiceProvider = (() => {
+  const DROPDOWN_SERVICE_PROVIDER = '@@DROPDOWN_SERVICE_PROVIDER'
+  return {
+    provide: (handler: { onClick: () => void }) => {
+      provide(DROPDOWN_SERVICE_PROVIDER, handler)
+    },
+    inject: () => {
+      return inject(DROPDOWN_SERVICE_PROVIDER) as { onClick: () => void }
+    }
+  }
+})()
 
 const ServiceComponent = defineComponent({
   props: {
@@ -81,6 +93,10 @@ const ServiceComponent = defineComponent({
       document.body.removeEventListener('mousedown', onMousedownDocument, true)
     })
 
+    DropdownServiceProvider.provide({
+      onClick: methods.hide
+    })
+
     return () => (
       <div class={classes.value} style={styles.value} ref={el}>
         {state.option.content()}
@@ -98,9 +114,21 @@ const DropdownOption = defineComponent({
       type: String
     }
   },
-  setup(props) {
+  emits: {
+    click: (e: MouseEvent) => true
+  },
+  setup(props, ctx) {
+    const { onClick: dropdownClickHandler } = DropdownServiceProvider.inject()
+
+    const handler = {
+      onClick: (e: MouseEvent) => {
+        ctx.emit('click', e)
+        dropdownClickHandler()
+      }
+    }
+
     return () => (
-      <div class="dropdown-option">
+      <div class="dropdown-option" onClick={handler.onClick}>
         <i class={`iconfont ${props.icon}`} />
         <span>{props.label}</span>
       </div>
